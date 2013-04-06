dca572d984ebc8e693e38c5f2d673f2010026c74
Alex Petersen <crono15@gmail.com>
2013-04-06 01:42:35 -0400

    Adding all files to GitHub
    I get it now... Windows edition works better. Need to figure out how to
    do that with VPS Server

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php 
+$flavors = array("Cake Batter","Cookie Dough");
+//$flavors["Cake Batter"] = array("test", "test1");
+//$flavors["Cookie Doughr"] = array("2test", "2test1");
+
+
+foreach ($flavors as $a => $b) {
+  //  echo $a;
+    echo $b;
+
+ //   exit;
+
+}
+
+?>
